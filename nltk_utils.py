3e7fa06e3721f3adf071d017b9f5c160006f2d94
GigaTasoulis <tasosgiann2001@gmail.com>
2024-03-04 19:33:33 +0200

    Implemented part of the Chatbot training

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,24 +1,41 @@
 import nltk
 # nltk.download('punkt') # package me pretrained tokenizer.
 from nltk.stem.porter import PorterStemmer # for stemming
+import numpy as np
+
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
 
 def stemming(word):
+    """
+    stemming = find the root form of the word
+    """
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence,all_words):
-    pass
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    tokenized_sentence = [stemming(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    
+    return bag
 
-
-
-a = "What is today's announcements?"
-print(a)
-a = tokenize(a)
-print(a)
-
-words = ["organize","organizes","organizing"]
-stemmed_words = [stemming(word) for word in words]
-print(stemmed_words)
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag   = bag_of_words(sentence,words)
+print(bag)
---
+++ train.py
@@ -0,0 +1,60 @@
+import json
+from nltk_utils import tokenize,stemming,bag_of_words
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+
+
+with open('data.json','r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+ignore_words = ['?','!','.',',']
+all_words = [stemming(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+print(tags)
+
+X_train = []
+Y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag) 
+    Y_train.append(label) # Check out later!
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+# Hyperparameters
+batch_size = 8
+
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size,shuffle=True, num_workers=2)
+
