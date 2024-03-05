96d33f8644ead5c3b314f26acc87ba7f0a04b97b
GigaTasoulis <tasosgiann2001@gmail.com>
2024-03-05 18:57:35 +0200

    Basic Display implemntation

---
+++ static/app.js
@@ -0,0 +1,59 @@
+class Chatbox{
+    constructor(){
+        this.args = {
+            openButton: document.querySelector('.chatbox_button'),
+            chatbox: document.querySelector('.support'),
+            sendButton: document.querySelector('.chatbox_send_button')
+        }
+        
+        this.state = false;
+        this.messages = [];
+
+    }
+
+    display() {
+        const {openButton, chatBox, sendButton} = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox))
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({key}) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        // show or hides the box
+        if(this.state) {
+            chatbox.classList.add('chatbox--active')
+        } else {
+            chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    updateChatText(chatbox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item, index) {
+            if (item.name === "Sam")
+            {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
+            }
+            else
+            {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+            }
+          });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+
+const chatbox = new Chatbox();
+chatbox.display();
