3296d937a05ada21b937b76da8b7aa7687fd35ba
GigaTasoulis <tasosgiann2001@gmail.com>
2024-03-24 21:33:07 +0200

    Fixed minor bugs

--- static/app.js
+++ static/app.js
@@ -8,8 +8,6 @@ class Chatbox {
         
         this.state = false;
         this.messages = [];
-        this.previousOptions = [];
-        this.roleButtons = []; // Array to store dynamically created role buttons
     }
 
     display() {
@@ -29,7 +27,6 @@ class Chatbox {
         // Dynamically create and append the STUDENT and PARENT buttons
         const studentButton = this.createRoleButton("ΦΟΙΤΗΤΗΣ");
         const parentButton = this.createRoleButton("ΓΟΝΕΑΣ");
-        this.roleButtons.push(studentButton, parentButton);
         chatbox.querySelector('.chatbox__messages').appendChild(studentButton);
         chatbox.querySelector('.chatbox__messages').appendChild(parentButton);
     }
@@ -101,29 +98,8 @@ class Chatbox {
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
     }
-
-    handleRoleSelection(selectedRole) {
-        if (selectedRole === 'ΦΟΙΤΗΤΗΣ') {
-            const studentOptions = [
-                'ΤΟ ΤΜΗΜΑ', 
-                'ΕΚΠΑΙΔΕΥΣΗ', 
-                'ΠΟΙΟΤΗΤΑ', 
-                'ΕΡΕΥΝΑ', 
-                'ΠΡΟΣΩΠΙΚΟ', 
-                'ΑΝΑΚΟΙΝΩΣΕΙΣ',
-                'ΠΡΟΓΡΑΜΜΑ ΜΑΘΗΜΑΤΩΝ'
-            ];
-            this.displayOptions(selectedRole, studentOptions);
-        } else if (selectedRole === 'ΓΟΝΕΑΣ') {
-            const parentOptions = [
-                'ΕΠΙΚΟΙΝΩΝΙΑ ΜΕ ΚΑΘΗΓΗΤΕΣ', 
-                'ΓΙΑ ΤΟ ΤΜΗΜΑ'
-            ];
-            this.displayOptions(selectedRole, parentOptions);
-        }
-    }
-
-    displayOptions(selectedRole, options) {
+    
+    displayOptions(selectedOption,options) {
         const chatboxMessages = document.querySelector('.chatbox__messages');
         chatboxMessages.innerHTML = ''; // Clear previous messages
     
@@ -142,55 +118,148 @@ class Chatbox {
             optionButton.textContent = option;
             optionButton.addEventListener('click', () => {
                 const selectedOption = optionButton.textContent;
-                this.previousOptions.push(selectedOption);
                 this.handleOptionSelection(selectedOption);
             });
             chatboxMessages.appendChild(optionButton);
         });
     }
+    //STUDENT OR PARENT
+    handleRoleSelection(selectedRole) {
 
-    handleOptionSelection(selectedOption) {
-        console.log('Selected Option:', selectedOption);
-        switch(selectedOption) {
-            case 'ΤΟ ΤΜΗΜΑ':
-                window.location.href = 'https://www.ece.upatras.gr/index.php/el/hm-ty.html';
+        // Display the options based on the selected role
+        switch(selectedRole) {
+            case 'ΦΟΙΤΗΤΗΣ':
+                console.log("Student role selected.");
+                this.handleStudentOptions();
                 break;
-            case 'ΕΚΠΑΙΔΕΥΣΗ':
-                window.location.href = 'https://www.ece.upatras.gr/index.php/el/mm-education.html';
+            case 'ΓΟΝΕΑΣ':
+                console.log("Parent role selected.");
+                this.handleParentOptions();
                 break;
-            case 'ΠΡΟΓΡΑΜΜΑ ΜΑΘΗΜΑΤΩΝ': // TODO fix the fetcing.
-                const pdfUrl = 'https://www.ece.upatras.gr/images/Attachments/%CE%A0%CF%81%CF%8C%CE%B3%CF%81%CE%B1%CE%BC%CE%BC%CE%B1_%CE%9C%CE%B1%CE%B8%CE%B7%CE%BC%CE%AC%CF%84%CF%89%CE%BD-%CE%95%CE%BE%CE%B5%CF%84%CE%AC%CF%83%CE%B5%CF%89%CE%BD/earino202324v1.pdf';
-        fetch(pdfUrl)
-            .then(response => response.blob())
-            .then(blob => {
-                // Create a URL for the PDF blob
-                const pdfUrl = URL.createObjectURL(blob);
-                // Display the PDF link in the chatbox
-                const pdfLink = document.createElement('a');
-                pdfLink.href = pdfUrl;
-                pdfLink.textContent = 'Πρόγραμμα Μαθημάτων';
-                pdfLink.target = '_blank'; // Open in a new tab
-                const chatboxMessages = document.querySelector('.chatbox__messages');
-                chatboxMessages.appendChild(pdfLink);
-            })
-            .catch(error => {
-                console.error('Error fetching PDF:', error);
-            });
             default:
                 break;
         }
     }
 
+    // FOR THE STUDENT OPTION
+    handleStudentOptions() {
+        console.log("Handling student options...");
+        const studentOptions = [
+            'ΤΟ ΤΜΗΜΑ', 
+            'ΕΚΠΑΙΔΕΥΣΗ', 
+            'ΠΟΙΟΤΗΤΑ', 
+            'ΕΡΕΥΝΑ', 
+            'ΠΡΟΣΩΠΙΚΟ', 
+            'ΑΝΑΚΟΙΝΩΣΕΙΣ',
+            'ΠΡΟΓΡΑΜΜΑ ΜΑΘΗΜΑΤΩΝ',
+            'ΠΡΟΟΔΟΣ'
+        ];
+        this.displayOptions(null, studentOptions);
+    }
+    // FOR THE PARENT OPTION
+    handleParentOptions() {
+        console.log("Handling parent options...");
+        const parentOptions = [
+            'ΤΟ ΤΜΗΜΑ', 
+            'ΠΟΙΟΤΗΤΑ', 
+            'ΑΝΑΚΟΙΝΩΣΕΙΣ'
+        ];
+        this.displayOptions(null, parentOptions);
+    }
+
+    handleOptionSelection(selectedOption) {
+        switch(selectedOption) {
+            case 'ΤΟ ΤΜΗΜΑ':
+                console.log("Department option selected.");
+                this.handleDepartmentOptions();
+                break;
+            case 'ΕΚΠΑΙΔΕΥΣΗ':
+                console.log("Education option selected.");
+                this.handleEducationOptions();
+                break;
+            case 'ΠΟΙΟΤΗΤΑ':
+                console.log("Quality option selected.");
+                this.handleQualityOptions();
+                break;
+            case 'ΕΡΕΥΝΑ':
+                console.log("Research option selected.");
+                this.handleResearchOptions();
+                break;
+            case 'ΠΡΟΣΩΠΙΚΟ':
+                console.log("Staff option selected.");
+                this.handleStaffOptions();
+                break;
+            case 'ΑΝΑΚΟΙΝΩΣΕΙΣ':
+                console.log("Announcements option selected.");
+                this.handleAnnouncementsOptions();
+                break;
+            case 'ΠΡΟΓΡΑΜΜΑ ΜΑΘΗΜΑΤΩΝ':
+                console.log("Schedule option selected.");
+                this.handleScheduleOptions();
+                break;
+            case 'ΠΡΟΟΔΟΣ':
+                console.log("Progress option selected.");
+                this.handleProgressOptions();
+                break;
+            default:
+                break;
+        }
+    }
+
+    handleDepartmentOptions() {
+        console.log("Handling department options...");
+        // Handle options under 'ΤΟ ΤΜΗΜΑ'
+    }
+    
+    handleEducationOptions() {
+        console.log("Handling education options...");
+        // Handle options under 'ΕΚΠΑΙΔΕΥΣΗ'
+    }
+    
+    handleQualityOptions() {
+        console.log("Handling quality options...");
+        // Handle options under 'ΠΟΙΟΤΗΤΑ'
+    }
+    
+    handleResearchOptions() {
+        console.log("Handling research options...");
+        // Handle options under 'ΕΡΕΥΝΑ'
+    }
+    
+    handleStaffOptions() {
+        console.log("Handling staff options...");
+        // Handle options under 'ΠΡΟΣΩΠΙΚΟ'
+    }
+    
+    handleAnnouncementsOptions() {
+        console.log("Handling announcements options...");
+        // Handle options under 'ΑΝΑΚΟΙΝΩΣΕΙΣ'
+    }
+    
+    handleScheduleOptions() {
+        console.log("Handling schedule options...");
+    }
+    
+    handleProgressOptions() {
+        console.log("Handling progress options...");
+        const progressOptions = [
+            'Προσπέλαση του Progress',
+            'Άλλο'
+        ];
+        this.displayOptions(null, progressOptions);
+    }
+
     goBack() {
+        console.log("Going back...");
         const chatboxMessages = document.querySelector('.chatbox__messages');
         chatboxMessages.innerHTML = ``;
-        this.roleButtons.forEach(button => {
-            const role = button.dataset.role;
-            if (role === 'ΦΟΙΤΗΤΗΣ' || role === 'ΓΟΝΕΑΣ') {
-                chatboxMessages.appendChild(button);
-            }
-        });
+        this.display();
+        
     }
+    
+    
+    
+        
 }
 
 const chatbox = new Chatbox();
